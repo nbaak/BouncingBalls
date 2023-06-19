8fb043bf1819e1f1383fe3fb64837ce58bac282d
nbaak <niklas.baak@outlook.com>
2023-06-19 19:44:21 +0200

    adding balls with click and gravity config

--- src/simmulation.py
+++ src/simmulation.py
@@ -47,7 +47,7 @@ def main():
         radius = randint(20, 50)
         balls.append(Ball(pos, speed, radius))
 
-    gravity_enabled = config.GRAVITY
+    gravity_enabled = True
 
     while True:
         for event in pygame.event.get():
@@ -77,10 +77,10 @@ def main():
                     exit()
             elif event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:  # Left mouse button
-                    pos = list(pygame.mouse.get_pos())
+                    pos = pygame.mouse.get_pos()
                     speed = [randint(-5, 5), randint(-5, 5)]
                     radius = randint(20, 50)
-                    balls.append(Ball(pos, speed, radius))
+                    balls.append(Ball(list(pos), speed, radius))
 
         # Clear the screen
         screen.fill(colors.WHITE)
